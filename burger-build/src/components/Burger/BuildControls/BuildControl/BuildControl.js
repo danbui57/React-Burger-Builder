2e4293583ac0855497fe61b9f27d92c52a42fca7
danbui57 <danbui57@gmail.com>
2020-02-18 17:26:30 -0500

    'adds the build control component

---
+++ burger-build/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from ".BuildControl.Module.css"
+
+const buildControl = props => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={More}>More</button>
+    </div>
+);
+ 
+export default buildControl;
