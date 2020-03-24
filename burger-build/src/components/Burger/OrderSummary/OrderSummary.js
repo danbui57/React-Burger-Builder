590f1595d309a75ee7036feb5ce7532e6a995b35
danbui57 <danbui57@gmail.com>
2020-03-23 21:45:08 -0400

    Sending a Post Request

--- burger-build/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-build/src/components/Burger/OrderSummary/OrderSummary.js
@@ -27,7 +27,7 @@ class OrderSummary extends React.Component {
             <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
                     <p>Continue to Checkout?</p>
                     <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
-                    <Button btnType="Success" clicked={this.purchaseContinued}>CONTINUE</Button>
+                    <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
                 </Aux>
             );
             
--- burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
+import axios from "../../axios-orders"
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -75,9 +76,27 @@ class BurgerBuilder extends React.Component {
 
             purchaseCancelHandler = () => {
                 this.setState({purchasing: false})
+
             };
             purchaseContinueHandler = () => {
-                alert("You continue!")
+                // alert("You continue!")
+                const order = {
+                    ingredients: this.state.ingredients,
+                    price: this.state.totalPrice,
+                    customer: {
+                        name: "Daniel Buitrago",
+                        address: {
+                            street: "Delevan st",
+                            zipCode: "30325",
+                            country: "USA"
+                    },
+                    email: "danbui57@test.com",
+                },
+                deliveryMethod: "fastest"
+            }      
+                axios.post("/orders.json", order)
+                .then(response => console.log(response))
+                .catch(error => console.log(error));
             };
 
     render() {
