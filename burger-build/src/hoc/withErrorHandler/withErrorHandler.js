44546491df8484d0cc8be1833f3d4cc425c777d0
danbui57 <danbui57@gmail.com>
2020-04-06 21:59:39 -0400

    Building the Checkout Container

--- burger-build/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-build/src/hoc/withErrorHandler/withErrorHandler.js
@@ -34,7 +34,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
                     <Modal show={this.state.error}
                         modalClosed={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
-                        Something didn't work!
             </Modal>
                     <WrappedComponent {...this.props} />
                 </Aux>
