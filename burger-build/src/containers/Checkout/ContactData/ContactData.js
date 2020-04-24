883ebe29248e5ca410aec2d0bbad50e9fc50eceb
danbui57 <danbui57@gmail.com>
2020-04-23 21:26:56 -0400

    Setting Up a JS Config for the Form

--- burger-build/src/containers/Checkout/ContactData/ContactData.js
+++ burger-build/src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,54 @@ import Input from "../../../components/UI/Input/Input"
 
 class ContactData extends React.Component {
     state = {
-        name: "",
-        email: "",
-        address: {
-            street: "",
-            postalCode: ""
+        orderForm: {
+            name: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Your Name"
+                },
+                value: ""
+            },
+            street: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Street"
+                },
+                value: ""
+            },
+            zipCode: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "ZIP Code"
+                },
+            },
+
+            country: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "country"
+                },
+                value: ""
+            },
+            email: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Your E-mail"
+                },
+            },
+            deliveryMethod:{
+                elementType: "select",
+                elementConfig: {
+                    options: [{value: "Fastest", displayValue: "Fastest" },
+                              {vale: "Cheapest", displayValue: "Cheapest"}]
+                }
+            }
+
         },
         loading: false
     }
@@ -23,42 +66,33 @@ class ContactData extends React.Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: "Daniel Buitrago",
-                address: {
-                    street: "Delevan st",
-                    zipCode: "30325",
-                    country: "USA"
-                },
-                email: "danbui57@test.com",
-            },
-            deliveryMethod: "fastest"
+
         }
         axios.post("/orders.json", order)
             .then(response => {
-                this.setState({ loading: false});
+                this.setState({ loading: false });
                 this.props.history.push("/");
             })
             .catch(error => {
-                this.setState({ loading: false});
+                this.setState({ loading: false });
             });
     }
 
-    render () {
+    render() {
         let form = (
             <form>
-                    <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                    <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                    <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                    <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                <Input elementType="..." elementConfig="..." value="..."/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
         );
         if (this.state.loading) {
             form = <Spinner />
         }
         return (
-            <div className={Classes.ContactData }>
+            <div className={Classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
                 {form}
             </div>
