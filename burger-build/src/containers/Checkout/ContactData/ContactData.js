321eefbf72ba144a56eed5d892a7c1ea23082949
danbui57 <danbui57@gmail.com>
2020-05-02 22:18:41 -0400

    Adding custom form validation

--- burger-build/src/containers/Checkout/ContactData/ContactData.js
+++ burger-build/src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends React.Component {
                     type: "text",
                     placeholder: "Your Name"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: "input",
@@ -22,7 +26,11 @@ class ContactData extends React.Component {
                     type: "text",
                     placeholder: "Street"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: "input",
@@ -30,7 +38,13 @@ class ContactData extends React.Component {
                     type: "text",
                     placeholder: "ZIP Code"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
 
             country: {
@@ -39,7 +53,11 @@ class ContactData extends React.Component {
                     type: "text",
                     placeholder: "Country"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: "input",
@@ -47,7 +65,11 @@ class ContactData extends React.Component {
                     type: "email",
                     placeholder: "Your E-Mail"
                 },
-                value: ""
+                value: "",
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: "select",
@@ -66,11 +88,15 @@ class ContactData extends React.Component {
 
     orderHandler = (event) => {
         event.preventDefault()
-        //  alert("You continue!")
         this.setState({ loading: true })
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
 
         }
         axios.post("/orders.json", order)
@@ -83,6 +109,23 @@ class ContactData extends React.Component {
             });
     }
 
+    checkValidity(value, rules) {
+        let isValid = false;
+
+        if (rules.required) {
+            isValid = value.trim() !== "";
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -91,7 +134,9 @@ class ContactData extends React.Component {
            ...updatedOrderForm[inputIdentifier]
     };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement)
         this.setState({orderForm: updatedOrderForm})
     }
 
@@ -104,7 +149,7 @@ class ContactData extends React.Component {
             });
         }
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
